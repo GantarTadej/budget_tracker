ebda994f4dc49feb29ad88c2abe0360682962041
Tadej <tadej.gantar@gmail.com>
2026-02-03 02:58:57 +0100

    Add complete budget tracker with menu

--- budget.py
+++ budget.py
@@ -9,10 +9,57 @@ def load_expenses():
         return []
     
 #odpre stroške za pisanje
-
-def save_expenses():
+def save_expenses(expenses):
     with open('expenses.json', 'w') as file:
         json.dump(expenses, file, indent = 4)
 
+#dodaj strošek
+def add_expense(expenses):
+    amount = float(input("Vnesi vsoto: "))
+    category = input("Vnesi kategorijo: (Hrana, Najemnina, Avto, drugo): ")
+    description = input("Opis: ") 
 
-        
+    expense = {
+        "amount": amount,
+        "category": category,
+        "description": description
+    }    
+
+    expenses.append(expense)
+    save_expenses(expenses)
+    print("Strošek uspešno dodan!!")
+
+#Preglej Stroške
+def view_expenses(expenses):
+    if not expenses:
+        print("Zaenkrat nimaš stroškov!")
+        return 
+    
+    print("\n--- Tvoji Stroški ---")
+    for i, expense in enumerate(expenses, 1):
+        print(f"{i}. {expense['description']} - {expense['amount']:.2f} ({expense['category']})")
+
+#glavni meni
+
+def main():
+    expenses = load_expenses()
+
+    while True:
+        print("\n=== Budget Tracker ===")
+        print("1. Dodaj Strošek")
+        print("2. Preglej Stroške")
+        print("3. Izhod")
+
+        choice = input("\nIzberi Opcijo (1-3): ")
+
+        if choice == '1':
+            add_expense(expenses)
+        elif choice == '2':
+            view_expenses(expenses)
+        elif choice == '3':
+            break
+        else:
+            print("Napačna izbira, poskusi znova.")
+
+if __name__ == "__main__":
+    main()
